c14c16f8f46cc74ba9627afed98f36991751825a
Hoang Viet <thvroyal@gmail.com>
2023-06-08 18:42:06 +0700

    add: set secure cookie

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -77,14 +77,14 @@ const generateAuthTokens = async (user, res) => {
     res.cookie('refreshToken', refreshToken, {
       maxAge: config.jwt.refreshExpirationDays * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'none',
     });
 
     res.cookie('accessToken', accessToken, {
       maxAge: config.jwt.accessExpirationMinutes * 60 * 1000,
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'none',
     });
   }
